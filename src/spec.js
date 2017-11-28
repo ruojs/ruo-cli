fbbdc531c97ea1bbda594fd1325486490864dfd6
Gaara <sabakugaara@users.noreply.github.com>
2017-11-28 10:56:45 +0800

    feat `ruo spec` now generate file cache

--- src/spec.js
+++ src/spec.js
@@ -1,8 +1,20 @@
-const {parseAsync} = require('ruo')
+const fs = require('fs')
+const {rc} = require('ruo')
+const parser = require('ruo-swagger-parser')
 
 module.exports = () => {
-  parseAsync().then((spec) => {
-    console.log(JSON.stringify(spec, null, 2)); // eslint-disable-line
+  const root = rc.source
+  const cachePath = root + '/spec/swagger.json'
+
+  parser(
+    root,
+    '**/*' + rc.suffix.spec,
+    rc.suffix.spec,
+    'api',
+    rc.shadow
+  ).then((spec) => {
+    fs.writeFileSync(cachePath, JSON.stringify(spec, null, 2), 'utf8')
+    console.log(`generate swagger.json in ${cachePath} success`); // eslint-disable-line
   }).catch((err) => {
     console.log(err.stack); // eslint-disable-line
   })
